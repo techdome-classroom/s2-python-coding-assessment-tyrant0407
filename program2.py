50779270c23812ca528cba9f49f6be5a640dc28d
tyrant0407 <amankushwaha1331@gmail.com>
2024-09-27 14:40:08 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -18,6 +18,10 @@ class Solution(object):
         for char in s:
             value=roman_map[char]
             if value>prev_value:
+                result+=value-2*prev_value
+            else:
+                result+=value
+            
 
     pass
 
