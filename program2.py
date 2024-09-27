3118de0dbc824799c24cdd20027dc554ed1dd369
tyrant0407 <amankushwaha1331@gmail.com>
2024-09-27 14:41:54 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,25 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        # roman_map={
-        #     'I':1,
-        #     'V':5,
-        #     'X':10,
-        #     'L':50,
-        #     'C':100,
-        #     'D':500,
-        #     'M':1000,
-        # }
-        # result=0
-        # prev_value=0
-        # for char in s:
-        #     value=roman_map[char]
-        #     if value>prev_value:
-        #         result+=value-2*prev_value
-        #     else:
-        #         result+=value
-        #     prev_value=value
-        # return result
+        roman_map={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000,
+        }
+        result=0
+        prev_value=0
+        for char in s:
+            value=roman_map[char]
+            if value>prev_value:
+                result+=value-2*prev_value
+            else:
+                result+=value
+            prev_value=value
+        return result
 
     pass
 
